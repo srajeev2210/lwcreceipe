c9f41f453722b886e59814479f03db76efdbc62a
Rajeevan S <srajeev2210@gmail.com>
2020-09-26 20:24:01 +0530

    LightningDataService-1 - 26Sept

---
+++ force-app/main/default/lwc/ldsCreateRecord/ldsCreateRecord.js
@@ -0,0 +1,43 @@
+import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+
+export default class LdsCreateRecord extends LightningElement {
+    accountId
+    accountName = ''
+
+    handleChange(event){
+        let name = event.target.value
+        console.log(name)
+        this.accountId = undefined
+        this.accountName = event.target.value
+    }
+
+    createAccount() {
+        const fields = {};
+        fields[NAME_FIELD.fieldApiName] = this.accountName;
+        const recordInput = { apiName: ACCOUNT_OBJECT.objectApiName, fields };
+        createRecord(recordInput)
+            .then((account) => {
+                this.accountId = account.id;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Account created',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: reduceErrors(error).join(', '),
+                        variant: 'error'
+                    })
+                );
+            });
+    }
+}
