6008a5ebfcfc5890f4c7fafc5e4e2f23583a8640
Rajeevan S <srajeev2210@gmail.com>
2020-09-28 13:38:28 +0530

    RecordFormDynamicContact - 28Sept

---
+++ force-app/main/default/lwc/recordFormDynamicContact/recordFormDynamicContact.js
@@ -0,0 +1,10 @@
+import { LightningElement, api } from 'lwc';
+
+export default class RecordFormDynamicContact extends LightningElement {
+
+    @api recordId;
+    @api objectApiName;
+
+    fields = ['AccountId', 'Name', 'Title','Phone','Email'];
+    
+}
